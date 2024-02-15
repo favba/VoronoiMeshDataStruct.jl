c9224a2967560ae0581332b8d4ef80ca565c40c3
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2024-02-15 11:27:54 -0300

    Added `getproperty` compatibility with MPAS names for vertex data struct.

--- src/vertices_datastruct.jl
+++ src/vertices_datastruct.jl
@@ -7,6 +7,11 @@ end
 
 integer_precision(::Type{<:VertexConnectivity{T}}) where T = T
 
+Base.getproperty(vertex::VertexConnectivity,s::Symbol) = _getproperty(vertex,Val(s))
+_getproperty(vertex::VertexConnectivity,::Val{s}) where s = getfield(vertex,s)
+_getproperty(vertex::VertexConnectivity,::Val{:edgesOnVertex}) = getfield(vertex,:edges)
+_getproperty(vertex::VertexConnectivity,::Val{:cellsOnVertex}) = getfield(vertex,:cells)
+
 struct VertexBase{TI<:Integer, VAPos<:VecArray{<:Any,1}}
     """Vertices connectivity data struct"""
     indices::VertexConnectivity{TI}
@@ -16,6 +21,14 @@ end
 
 integer_precision(::Type{<:VertexBase{T}}) where T = T
 float_precision(::Type{<:VertexBase{T,V}}) where {T,V} = TensorsLite._my_eltype(eltype(V))
+
+Base.getproperty(vertex::VertexBase,s::Symbol) = _getproperty(vertex,Val(s))
+_getproperty(vertex::VertexBase,::Val{s}) where s = getfield(vertex,s)
+_getproperty(vertex::VertexBase,::Val{:edgesOnVertex}) = getfield(vertex,:indices).edges
+_getproperty(vertex::VertexBase,::Val{:cellsOnVertex}) = getfield(vertex,:indices).cells
+_getproperty(vertex::VertexBase,::Val{:xVertex}) = getfield(vertex,:position).x
+_getproperty(vertex::VertexBase,::Val{:yVertex}) = getfield(vertex,:position).y
+_getproperty(vertex::VertexBase,::Val{:zVertex}) = getfield(vertex,:position).z
 
 mutable struct VertexInfo{TVertex<:VertexBase,TI,TF}
     const base::TVertex
@@ -38,8 +51,19 @@ end
 integer_precision(::Type{<:VertexInfo{TV,TI}}) where {TV,TI} = TI
 float_precision(::Type{<:VertexInfo{TV,TI,TF}}) where {TV,TI,TF} = TF
 
-@inline Base.getproperty(vertex::VertexInfo,s::Symbol) = _getproperty(vertex,Val(s))
+Base.getproperty(vertex::VertexInfo,s::Symbol) = _getproperty(vertex,Val(s))
 
-@inline _getproperty(vertex::VertexInfo,::Val{s}) where s = getfield(vertex,s)
-@inline _getproperty(vertex::VertexInfo,::Val{:indices}) = getfield(vertex,:base).indices
-@inline _getproperty(vertex::VertexInfo,::Val{:position}) = getfield(vertex,:base).position
+_getproperty(vertex::VertexInfo,::Val{s}) where s = getfield(vertex,s)
+_getproperty(vertex::VertexInfo,::Val{:indices}) = getfield(vertex,:base).indices
+_getproperty(vertex::VertexInfo,::Val{:position}) = getfield(vertex,:base).position
+
+for s in (:cellsOnVertex,:edgesOnVertex,:xVertex,:yVertex,:zVertex)
+    @eval _getproperty(vertex::VertexInfo,::Val{$(QuoteNode(s))}) = getproperty(getfield(vertex,:base),$(QuoteNode(s)))
+end
+
+for (s,nc) in pairs((longitude=:lonVertex, latitude=:latVertex,
+                     indexToID=:indexToVertexID, area=:areaTriangle,
+                     bdyMask=:bdyMaskVertex, kiteAreas=:kiteAreasOnVertex))
+
+    @eval _getproperty(vertex::VertexInfo,::Val{$(QuoteNode(nc))}) = getfield(vertex,$(QuoteNode(s)))
+end
